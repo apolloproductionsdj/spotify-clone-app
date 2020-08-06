7dacaec8723cc31adb4c5f613323e56800d38857
apolloproductionsdj <apolloproductionsdj@gmail.com>
2020-08-05 23:34:39 -0700

    added DataLayer by utilizing React Context API

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
 
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState(null); 
+  const [{}, dispatch] = useDataLayerValue();
+
   // Run code given on a given condition
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -28,7 +32,9 @@ function App() {
   }, []);
 
   return (
-    <div className="app">{token ? <h1>I am logged in</h1> : <Login />}</div>
+    <div className="app">{token ? 
+      <Player />
+      : <Login />}</div>
   );
 }
 
