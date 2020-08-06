c969707ac7aebf249a921e4756eb4d11d56c0402
apolloproductionsdj <apolloproductionsdj@gmail.com>
2020-08-05 23:59:22 -0700

    setup App.js to also get the token from the user to stay logged in

--- src/reducer.js
+++ src/reducer.js
@@ -2,7 +2,9 @@ export const initialState = {
   user: null,
   playlists: [],
   playing: false,
-  item: null
+  item: null,
+  // REMOVE after finished developing...
+  token: "BQBCkA0C0ZsUpnNzFPHGl-o6ks9lIm_UbQqE6Xawoxd1jCEdQ8NyfMgsOtY0ACXPZKHTJvgqnc8lzSCF4OH_oNR3PdxXLHs3t-xmfwYu_dPh5RFGQ95asKk2kBC08p9p7MTQpYjUqil3VI6NrdiZGPp-kWo"
 }
 
 const reducer = (state, action) => {
@@ -10,9 +12,14 @@ console.log(action);
 
   switch (action.type) {
     case 'SET_USER':
-      return {
+      return { 
         ...state,
         user: action.user 
+      };
+    case 'SET_TOKEN':
+      return {
+        ...state,
+        token: action.token 
       };
     default: 
       return state;
