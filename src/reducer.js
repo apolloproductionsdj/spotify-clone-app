1f260f7bf07d5b2145c3d7f6a9caf0eb1be2a57f
apolloproductionsdj <apolloproductionsdj@gmail.com>
2020-08-06 13:49:33 -0700

    finished footer but without playing functionality

--- src/reducer.js
+++ src/reducer.js
@@ -4,7 +4,7 @@ export const initialState = {
   playing: false,
   item: null,
   // REMOVE after finished developing...
-  // token: "BQBCkA0C0ZsUpnNzFPHGl-o6ks9lIm_UbQqE6Xawoxd1jCEdQ8NyfMgsOtY0ACXPZKHTJvgqnc8lzSCF4OH_oNR3PdxXLHs3t-xmfwYu_dPh5RFGQ95asKk2kBC08p9p7MTQpYjUqil3VI6NrdiZGPp-kWo"
+  token: "BQBCkA0C0ZsUpnNzFPHGl-o6ks9lIm_UbQqE6Xawoxd1jCEdQ8NyfMgsOtY0ACXPZKHTJvgqnc8lzSCF4OH_oNR3PdxXLHs3t-xmfwYu_dPh5RFGQ95asKk2kBC08p9p7MTQpYjUqil3VI6NrdiZGPp-kWo"
 };
 
 const reducer = (state, action) => {
